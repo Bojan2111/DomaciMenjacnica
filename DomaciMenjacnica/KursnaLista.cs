51310e06c72ec50cfd595937e385a1d32d81a436
Bojan Adzic <bojan.adzic@gmail.com>
2022-12-23 23:28:00 +0100

    Add project files.

---
+++ DomaciMenjacnica/KursnaLista.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomaciMenjacnica
+{
+    internal class KursnaLista
+    {
+        public string DatumListe { get; set; }
+        public List<Dictionary<string, double[]>> ListaValuta { get; set; }
+
+        public KursnaLista(string datumListe, List<Dictionary<string, double[]>> listaValuta)
+        {
+            this.DatumListe = datumListe;
+            ListaValuta = listaValuta;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] nizDatuma = DatumListe.Split('-');
+
+            sb.Append($"Kursna lista kreirana na dan {nizDatuma[2]}. {nizDatuma[1]}. {nizDatuma[0]}.\n" +
+                $"Oznaka | Kupovni | Prodajni | Srednji\n");
+            foreach(Dictionary<string, double[]> dict in ListaValuta)
+            {
+                foreach(KeyValuePair<string, double[]> kl in dict)
+                {
+                    sb.Append($"  {kl.Key}  | {kl.Value[0]} | {kl.Value[1]} | {kl.Value[2]}\n");
+                }
+            }
+            return sb.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is KursnaLista))
+                return false;
+            KursnaLista kl = (KursnaLista)obj;
+            return DatumListe.Equals(kl.DatumListe);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ DomaciMenjacnica/Valuta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomaciMenjacnica
+{
+    internal class Valuta
+    {
+        public string OznakaValute { get; set; }
+        public string NazivValute { get; set; }
+
+        public Valuta(string oznakaValute, string nazivValute)
+        {
+            OznakaValute = oznakaValute;
+            NazivValute = nazivValute;
+        }
+
+        public override string ToString()
+        {
+            return $"Valuta sa oznakom {OznakaValute} nosi naziv {NazivValute}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Valuta))
+                return false;
+            Valuta v = (Valuta)obj;
+            return OznakaValute.Equals(v.OznakaValute);
+        }
+
+        public override int GetHashCode()
+        {
+            return OznakaValute.GetHashCode() ^ NazivValute.GetHashCode();
+        }
+    }
+}
